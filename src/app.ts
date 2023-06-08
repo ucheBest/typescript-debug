5615c8b3f34479551d6bfbe6ccb928123538cf5d
Uchechi Akah <akah.uchechi@gmail.com>
2023-06-08 18:15:00 +0100

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { practice } from "./hello";
+
+class App {
+  /** Entry point of our app */
+  public static start() {
+    practice();
+  }
+}
+
+App.start();
---
+++ src/hello.ts
@@ -0,0 +1,18 @@
+/** Say hello */
+
+export function practice() {
+
+
+
+for( let i = 0; i< 4; i++){
+  console.log("+-+-+-+-+-+-+-+-")
+}
+
+
+
+
+
+
+}
+
+
